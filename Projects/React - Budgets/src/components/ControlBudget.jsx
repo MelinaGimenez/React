8fa85f50439d2f30a7d3fe170c45bb4a1640a992
MelinaGimenez <gimenez.melina@hotmail.com>
2023-03-28 21:19:39 -0300

    arreglando css y detalles

--- Projects/React - Budgets/src/components/ControlBudget.jsx
+++ Projects/React - Budgets/src/components/ControlBudget.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const ControlBudget = ({budget}) => {
+
+  //funcion para formatear a pesos
+  const formatQuantity = (amount) => {
+    return amount.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+  }
+
   return (
     <div className='container-budget container shadow two-columns'>
         <div>
@@ -8,7 +17,13 @@ const ControlBudget = ({budget}) => {
         </div>
         <div className='container-budget'>
             <p>
-                <span>Presupuesto: </span> ${budget}
+                <span>Presupuesto: </span> {formatQuantity(budget)}
+            </p>
+            <p>
+                <span>Disponible: </span> {formatQuantity(0)}
+            </p>
+            <p>
+                <span>Gastado: </span> {formatQuantity(0)}
             </p>
         </div>
     </div>
