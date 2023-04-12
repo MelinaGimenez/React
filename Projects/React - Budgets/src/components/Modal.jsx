5d027b3b5e9fa02d8dc71ac69d50a280282a8aa4
MelinaGimenez <gimenez.melina@hotmail.com>
2023-04-12 20:42:44 -0300

    continuando form gastos

--- Projects/React - Budgets/src/components/Modal.jsx
+++ Projects/React - Budgets/src/components/Modal.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 import closeBtn from '../img/cerrar.svg'
 
 const Modal = ({setModal, desingModal, setDesingModal}) => {
+
+    const [name, setName] = useState('')
+    const [amountForm, setAmountForm] = useState('')
+    const [category, setCategory] = useState('')
 
     //regresa estado original, //efecto y pantalla del + (agregar gasto) retirado
     const ocultarModal = () => {
@@ -23,6 +28,47 @@ const Modal = ({setModal, desingModal, setDesingModal}) => {
         </div>
         <form className={`form ${desingModal ? "desing" : 'close'}`}>
             <legend>Nuevo Gasto</legend>
+            <div className='camp'>
+                <label htmlFor="name">Nombre del Gasto</label>
+                <input 
+                    id='name' 
+                    type="text" 
+                    placeholder='Añade el nombre del Gasto'
+                    value={name}
+                    onChange={ e => setName(e.target.value)}
+                />
+            </div>
+            <div className='camp'>
+                <label htmlFor="amountForm">Cantidad</label>
+                <input 
+                    id='amountForm' 
+                    type="number" 
+                    placeholder='Añade la cantidad del Gasto: ej. 300' 
+                    value={amountForm}
+                    onChange={ e => setAmountForm(Number(e.target.value))}
+                />
+            </div>
+            <div className='camp'>
+                <label htmlFor="category">Categoria</label>
+                <select 
+                    id="category"                     
+                    value={category}
+                    onChange={ e => setCategory(e.target.value)}
+                >
+                    <option value="">-- Seleccione --</option>
+                    <option value="ahorro">Ahorro</option>
+                    <option value="comida">Comida</option>
+                    <option value="casa">Casa</option>
+                    <option value="gastos varios">Gastos varios</option>
+                    <option value="ocio">Ocio</option>
+                    <option value="salud">Salud</option>
+                    <option value="suscripciones">Suscripciones</option>
+                </select>
+            </div>
+            <input 
+                type="submit" 
+                value="Añadir Gasto" 
+            />
         </form>
     </div>
   )
