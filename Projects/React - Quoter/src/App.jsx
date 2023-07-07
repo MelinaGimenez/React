65a870115a70f7a652e552acd82068d03f669630
MelinaGimenez <gimenez.melina@hotmail.com>
2023-07-07 00:24:41 -0300

    comenzando css

--- Projects/React - Quoter/src/App.jsx
+++ Projects/React - Quoter/src/App.jsx
@@ -1,9 +1,57 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+
+//lista css
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px){
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    columns-gap: 2rem;
+  }
+`
+const Img = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2Fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
 
 function App() {
 
   return (
-    <h1>Desde App</h1>
+    <Container>
+
+      <Img
+        src={ImagenCripto}
+        alt="imagenes criptomonedas"
+      />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+      </div>
+
+    </Container>
   )
 }
 
